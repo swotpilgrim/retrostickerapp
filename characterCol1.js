99815de9d853da0543cbf3bafd3352cc6dce3123
swotpilgrim <robertbrown2@Gmail.com>
2025-05-20 22:51:14 +0100

    Add files via upload

---
+++ characterCol1.js
@@ -0,0 +1,26 @@
+// Column 1 data for Character mode - Main categories (Heading 2)
+const characterCol1 = {
+    "Vintage Americana": [
+        "Teen Culture Characters",
+        "Service Industry Characters",
+        "Music Scene Characters",
+        "Road Trip Characters",
+        "Home & Family Characters"
+    ],
+    
+    "Retro-Futurism": [
+        "Space Explorers & Travelers",
+        "Future Earth Civilians",
+        "Robots & Artificial Beings",
+        "Alien Species",
+        "Scientific Characters"
+    ],
+    
+    "Campy Gothic": [
+        "Classic Monster Types",
+        "Horror Professionals",
+        "B-Movie Character Types",
+        "Horror Host Characters",
+        "Occult Practitioners"
+    ]
+};
---
+++ characterCol2.js
@@ -0,0 +1,125 @@
+// Column 2 data for Character mode - Subcategories (Heading 3)
+const characterCol2 = {
+    // Vintage Americana character subcategories
+    "Teen Culture Characters": [
+        "Greaser Archetypes",
+        "Soda Shop Regulars",
+        "Car Culture Kids",
+        "School Stereotypes",
+        "Dance Scene Personalities"
+    ],
+    
+    "Service Industry Characters": [
+        "Diner Staff",
+        "Gas Station Attendants",
+        "Drive-In Workers",
+        "Retail Personalities",
+        "Entertainment Providers"
+    ],
+    
+    "Music Scene Characters": [
+        "Rock & Roll Rebels",
+        "Doo-Wop Groups",
+        "Radio Personalities",
+        "Dance Instructors",
+        "Record Store Characters"
+    ],
+    
+    "Road Trip Characters": [
+        "Tourist Family",
+        "Highway Businesses",
+        "Travel Companions",
+        "Regional Types",
+        "Roadside Helpers"
+    ],
+    
+    "Home & Family Characters": [
+        "Household Roles",
+        "Neighborhood Types",
+        "Family Members",
+        "Pet Companions",
+        "Visitors"
+    ],
+    
+    // Retro-Futurism character subcategories
+    "Space Explorers & Travelers": [
+        "Rocket Crew",
+        "Space Tourists",
+        "Orbital Workers",
+        "Interplanetary Diplomats",
+        "Space Military"
+    ],
+    
+    "Future Earth Civilians": [
+        "Commuters",
+        "Automated Home Family",
+        "Fashion Forward",
+        "Service Providers",
+        "Entertainment Figures"
+    ],
+    
+    "Robots & Artificial Beings": [
+        "Household Helpers",
+        "Specialized Workers",
+        "Companion Models",
+        "Humanoid Types",
+        "Rogue Units"
+    ],
+    
+    "Alien Species": [
+        "Friendly Visitors",
+        "Earth Observers",
+        "Interstellar Merchants",
+        "Alien Refugees",
+        "Exotic Beings"
+    ],
+    
+    "Scientific Characters": [
+        "Mad Scientists",
+        "Laboratory Teams",
+        "Inventors",
+        "Future Medical Staff",
+        "Science Heroes"
+    ],
+    
+    // Campy Gothic character subcategories
+    "Classic Monster Types": [
+        "Vampire Varieties",
+        "Werewolf Phases",
+        "Reanimated Beings",
+        "Ghosts & Spirits",
+        "Aquatic Horrors"
+    ],
+    
+    "Horror Professionals": [
+        "Mortuary Workers",
+        "Supernatural Investigators",
+        "Monster Hunters",
+        "Occult Specialists",
+        "Museum Curators"
+    ],
+    
+    "B-Movie Character Types": [
+        "Screaming Victims",
+        "Final Survivors",
+        "Campy Villains",
+        "Shocked Witnesses",
+        "Disbelieving Authorities"
+    ],
+    
+    "Horror Host Characters": [
+        "TV Presenters",
+        "Show Assistants",
+        "Studio Crew",
+        "Special Guests",
+        "Host Personas"
+    ],
+    
+    "Occult Practitioners": [
+        "Witches & Warlocks",
+        "Cult Members",
+        "Seance Participants",
+        "Curse Bearers",
+        "Dark Arts Students"
+    ]
+};
